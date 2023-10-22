ceecdfc7597089d846b1b8d08da7ffbef72fbacf
Bradley Li <jizhe.li@ucalgary.ca>
2023-10-21 23:27:25 -0600

    separated the board.js into boardSmall.js and boardLarge.js

---
+++ boardSmall.js
@@ -0,0 +1,8 @@
+/* Course: SENG 513 */
+/* Date: OCT 20, 2023 */
+/* Assignment 2 */
+/* Name: Jizhe Li */
+/* UCID: 30115255 */
+
+const size = 9;
+const board = Array(size).fill(-1); // initialize all the elements of the array to be -1
