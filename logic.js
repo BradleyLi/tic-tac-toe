6f893eb8ca73e9fdcc1e52bf50c3a88f530c27df
Bradley Li <jizhe.li@ucalgary.ca>
2023-10-21 23:34:57 -0600

    created the checkWInLarge function

--- logic.js
+++ logic.js
@@ -35,26 +35,36 @@ function checkWinSmall() {
   // if ((board[0] === 0 && board[1] === 0 && board[2] === 0) || (board[3] === 0 && board[4] === 0 && board[5] === 0)
   //   || (board[6] === 0 && board[7] === 0 && board[8] === 0)) {
   //   win = 0;
+  //   score_one++;
   // }
   // else if ((board[0] === 1 && board[1] === 1 && board[2] === 1) || (board[3] === 1 && board[4] === 1 && board[5] === 1)
   //   || (board[6] === 1 && board[7] === 1 && board[8] === 1)) {
   //   win = 1;
+  //   score_two++;
   // }
   // // check all of the vertical lines
   // else if ((board[0] === 0 && board[3] === 0 && board[6] === 0) || (board[1] === 0 && board[4] === 0 && board[7] === 0)
   //   || (board[2] === 0 && board[5] === 0 && board[8] === 0)) {
   //   win = 0;
+  //   score_one++;
   // }
   // else if ((board[0] === 1 && board[3] === 1 && board[6] === 1) || (board[1] === 1 && board[4] === 1 && board[7] === 1)
   //   || (board[2] === 1 && board[5] === 1 && board[8] === 1)) {
   //   win = 1;
+  //   score_two++;
   // }
   // // check all of the diagonal lines
   // else if ((board[0] === 0 && board[4] === 0 && board[8] === 0) || (board[2] === 0 && board[4] === 0 && board[6] === 0)) {
   //   win = 0;
+  //   score_one++;
   // }
   // else if ((board[0] === 1 && board[4] === 1 && board[8] === 1) || (board[2] === 1 && board[4] === 1 && board[6] === 1)) {
   //   win = 1;
+  //   score_two++;
   // }
+}
+
+// The function checkWin checks the win condition each time a move has been made
+function checkWinLarge() {
 }
 
